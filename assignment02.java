5ef4e7ff3aed12d9fcb27bc00bd12eda2b1c1a2a
josephsprott <josephsprott1@hotmail.com>
2024-09-19 13:40:50 -0600

    Added Assignment-02 Java file

---
+++ assignment02.java
@@ -0,0 +1,23 @@
+package assignment02;
+
+public class assignment02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
